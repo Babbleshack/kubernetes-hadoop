14bf74dfeed3f4b73b73d2ea150222933e435cf1
babbleshack <dcrl94@gmail.com>
2019-03-30 15:47:50 +0000

    Cleanup rubbish

--- delete-namespace.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-NAMESPACE=hadoop
-kubectl get namespace $NAMESPACE -o json |jq '.spec = {"finalizers":[]}' >temp.json
-curl -k -H "Content-Type: application/json" -X PUT --data-binary @temp.json 127.0.0.1:8001/api/v1/namespaces/$NAMESPACE/finalize
